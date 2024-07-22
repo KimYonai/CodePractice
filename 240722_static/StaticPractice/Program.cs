82afb77773dfdb3ccd90529c9e1b7e8bc8d6f670
KimYonai <msb0714@gmail.com>
2024-07-22 17:37:03 +0900

    fix_02_StaticPractice
    static 과제 수정본입니다.

--- 240722_static/StaticPractice/Program.cs
+++ 240722_static/StaticPractice/Program.cs
@@ -4,8 +4,19 @@ namespace StaticPractice
 {
     internal class Program
     {
-        class MyCalculator
+        public class MyCalculator
         {
+            public double x;
+            public double y;
+
+            public void SelectNum()
+            {
+                Console.Write("연산할 첫번재 숫자를 입력해주세요 : ");
+                double.TryParse(Console.ReadLine(), out x);
+                Console.Write("연산할 두번재 숫자를 입력해주세요 : ");
+                double.TryParse(Console.ReadLine(), out y);
+            }
+
             static double Add(double x, double y)
             {
                 return x + y;
@@ -41,6 +52,15 @@ namespace StaticPractice
                 }
 
                 return result;
+            }
+
+            public void PrintInfo()
+            {
+                Console.WriteLine($"덧셈 결과 : {Add(x, y)}");
+                Console.WriteLine($"뺄셈 결과 : {Subtract(x, y)}");
+                Console.WriteLine($"곱셈 결과 : {Multiply(x, y)}");
+                Console.WriteLine($"나눗셈 결과 : {Divide(x, y)}");
+                Console.WriteLine($"거듭제곱 결과 : {Squared(x, y)}");
             }
         }
         static void Main(string[] args)
